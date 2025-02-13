3cf5345e325d5a37f3fd81b7337ce1952b1209bf
Your Name <pratibhacodedev@gmail.com>
2025-02-13 22:07:05 +0530

    nestedFunction

---
+++ Function/checkBalance.js
@@ -0,0 +1,28 @@
+function createBankAccount(initialBalance) {
+    let balance = initialBalance; 
+
+    return {
+        deposit: function(amount) {
+            balance += amount;
+            return balance;
+        },
+        withdraw: function(amount) {
+            if (amount > balance) {
+                return "Insufficient funds";
+            }
+            balance -= amount;
+            return balance;
+        },
+        getBalance: function() {
+            return balance;
+        }
+    };
+}
+
+
+const account = createBankAccount(100);
+
+console.log(account.deposit(50));   
+console.log(account.withdraw(30));  
+console.log(account.getBalance());  
+console.log(account.withdraw(200)); 
