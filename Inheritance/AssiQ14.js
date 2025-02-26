10eb6cbe84a3a0263679a8e32a674a6772f46cbe
Your Name <pratibhacodedev@gmail.com>
2025-02-26 20:30:04 +0530

    Assigment @14

---
+++ Inheritance/AssiQ14.js
@@ -0,0 +1,21 @@
+function fetchData(){
+    return new Promise ((resolve,reject)=>{
+        setTimeout(()=>{
+            if(Math.random()>0.5){
+                resolve("Fetched data successfully!")
+            }else{
+                reject(new Error("Failed to fetch data"));
+            }
+        },1000)
+    })
+}
+
+async function fetchDataHandler(){
+    try{
+        const data = await fetchData();
+    }catch(error){
+        console.log("Error fetching data:", error.message);
+    }
+}
+
+fetchDataHandler()
